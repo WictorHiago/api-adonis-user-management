eb07ccd3581b1dcd1ecd56bfc24a4076b60fbfdb
WictorHiago <wictor.hsc@gmail.com>
2024-04-07 05:31:09 -0400

    fix: authentication, implement guard in request, need more refactor

--- api-adonis-user-management/start/routes.ts
+++ api-adonis-user-management/start/routes.ts
@@ -7,10 +7,53 @@
 |
 */
 
+import DashboardController from '#controllers/dashboard_controller'
+import User from '#models/user'
 import router from '@adonisjs/core/services/router'
+import { middleware } from '#start/kernel'
+import PostsController from '#controllers/posts_controller'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+
+router.post('/users', async ({ request }) => {
+  const data = request.only(['full_name', 'email', 'password'])
+  const user = await User.create(data)
+
+  return user
+})
+
+router.post('/users/:id/tokens', async ({ params }) => {
+  // const user = await User.findByOrFail(params.id)
+  const user = await User.findByOrFail('id', params.id)
+
+  const token = await User.accessTokens.create(user)
+
+  return {
+    type: 'bearer',
+    token: token.value!.release(),
+  }
+})
+
+router
+  .group(() => {
+    router.get('/dashboard', async () => {
+      return await User.all()
+    })
+  })
+  .prefix('/api')
+
+router.get('/dashboard', [DashboardController, 'index']).use(
+  middleware.auth({
+    guards: ['api'],
+  })
+)
+
+router.post('/post/add', [PostsController, 'store']).use(
+  middleware.auth({
+    guards: ['api'],
+  })
+)
